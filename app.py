3e91b3f72616d7b830ecdc5d4d2929da086eb2bb
pratie <155068699+pratie@users.noreply.github.com>
2024-10-08 04:03:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,4 @@ def read_table_as_dataframe(table_name, engine):
     print(f"\nSample data from {table_name}:")
     print(df.head())  # Print first 5 rows
     return df
+merged_df = events_df.merge(ci_df[['LOGICAL_NAME', 'TYPE']], on='LOGICAL_NAME', how='left')
